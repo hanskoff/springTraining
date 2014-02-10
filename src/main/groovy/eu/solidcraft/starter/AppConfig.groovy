ab9b516115a7cbc36bbe3de8f210ef7e11cfbaaf
Jan Kowalski <jan.kowalski@allegro.pl>
2014-02-10 14:58:31 +0100

    E3: Twice AgeScoringRule invocation

--- src/main/groovy/eu/solidcraft/starter/AppConfig.groovy
+++ src/main/groovy/eu/solidcraft/starter/AppConfig.groovy
@@ -2,22 +2,24 @@ package eu.solidcraft.starter
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 
-@Configuration
+//@Configuration
+@Component
 public class AppConfig {
 
-  @Bean(name="polish")
-  public ScoreCalculator scoreCalculatorPL(){
+  @Bean(name = "polish")
+  public ScoreCalculator scoreCalculatorPL() {
     return new ScoreCalculator(new PolishFraudDetector(), rulesList())
   }
 
-  @Bean(name="english")
-  public ScoreCalculator scoreCalculatorUK(){
+  @Bean(name = "english")
+  public ScoreCalculator scoreCalculatorUK() {
     return new ScoreCalculator(new EnglishFraudDetector(), rulesList())
   }
 
   @Bean
-  public List<ScoringRule> rulesList(){
+  public List<ScoringRule> rulesList() {
     return [new AgeScoringRule(), new LoansHistoryScoringRule(), new JobsScoringRule()]
   }
 }
